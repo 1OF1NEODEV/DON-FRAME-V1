b6bee0adfa0507c5571534f78a4c610a97a7f3a9
BoogalabsDev <1of1neoeth@gmail.com>
2025-01-10 05:25:01 -0600

    5

--- src/auth.ts
+++ src/auth.ts
@@ -11,67 +11,73 @@ declare module "next-auth" {
 }
 
 export const authOptions: AuthOptions = {
-    // Configure one or more authentication providers
-  providers: [
-    CredentialsProvider({
-      name: "Sign in with Farcaster",
-      credentials: {
-        message: {
-          label: "Message",
-          type: "text",
-          placeholder: "0x0",
-        },
-        signature: {
-          label: "Signature",
-          type: "text",
-          placeholder: "0x0",
-        },
-        // In a production app with a server, these should be fetched from
-        // your Farcaster data indexer rather than have them accepted as part
-        // of credentials.
-        name: {
-          label: "Name",
-          type: "text",
-          placeholder: "0x0",
-        },
-        pfp: {
-          label: "Pfp",
-          type: "text",
-          placeholder: "0x0",
-        },
-      },
-      async authorize(credentials, req) {
-        const csrfToken = req?.body?.csrfToken;
-        const appClient = createAppClient({
-          ethereum: viemConnector(),
-        });
+    secret: process.env.NEXTAUTH_SECRET,
+    providers: [
+        CredentialsProvider({
+            name: "Sign in with Farcaster",
+            credentials: {
+                message: {
+                    label: "Message",
+                    type: "text",
+                    placeholder: "0x0",
+                },
+                signature: {
+                    label: "Signature",
+                    type: "text",
+                    placeholder: "0x0",
+                },
+                // In a production app with a server, these should be fetched from
+                // your Farcaster data indexer rather than have them accepted as part
+                // of credentials.
+                name: {
+                    label: "Name",
+                    type: "text",
+                    placeholder: "0x0",
+                },
+                pfp: {
+                    label: "Pfp",
+                    type: "text",
+                    placeholder: "0x0",
+                },
+            },
+            async authorize(credentials, req) {
+                const csrfToken = req?.body?.csrfToken;
+                const appClient = createAppClient({
+                    ethereum: viemConnector(),
+                });
 
-        const verifyResponse = await appClient.verifySignInMessage({
-          message: credentials?.message as string,
-          signature: credentials?.signature as `0x${string}`,
-          domain: new URL(process.env.NEXTAUTH_URL ?? '').hostname,
-          nonce: csrfToken,
-        });
-        const { success, fid } = verifyResponse;
+                const verifyResponse = await appClient.verifySignInMessage({
+                    message: credentials?.message as string,
+                    signature: credentials?.signature as `0x${string}`,
+                    domain: new URL(process.env.NEXTAUTH_URL ?? '').hostname,
+                    nonce: csrfToken,
+                });
+                const { success, fid } = verifyResponse;
 
-        if (!success) {
-          return null;
-        }
+                if (!success) {
+                    return null;
+                }
 
-        return {
-          id: fid.toString(),
-        };
-      },
-    }),
-  ],
-  callbacks: {
-    session: async ({ session, token }) => {
-      if (session?.user) {
-        session.user.fid = parseInt(token.sub ?? '');
-      }
-      return session;
+                return {
+                    id: fid.toString(),
+                };
+            },
+        }),
+    ],
+    callbacks: {
+        session: async ({ session, token }) => {
+            if (session?.user) {
+                session.user.fid = parseInt(token.sub ?? '');
+            }
+            return session;
+        },
     },
-  }
+    session: {
+        strategy: "jwt",
+    },
+    pages: {
+        signIn: "/auth/signin",
+    },
 }
 
 export const getSession = () => getServerSession(authOptions)
